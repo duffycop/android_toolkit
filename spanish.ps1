e65db531e97894f60bbf9bb4866082c46ccc1ed1
Tino <cmartinpf@gmail.com>
2019-09-26 18:49:31 -0300

    Update spanish.ps1

--- spanish.ps1
+++ spanish.ps1
@@ -1,20 +1,20 @@
 #HOME VARS
 $var0 = "[0] - Salir"
-$var1 = "[1] - Instalar drivers"
+$var1 = "[1] - Instalar controladores"
 $var2 = "[2] - Extraer boot"
-$var3 = "[3] - Rootear"
-$var4 = "[4] - Desrootear"
+$var3 = "[3] - Instalar root"
+$var4 = "[4] - Desinstalar root"
 $varChoice = "Por favor, elije una opción"
 
 #VARS 1x
-$varInstalling = "Instalando drivers, por favor espere.."
+$varInstalling = "Instalando controladores, por favor espere.."
 
 #VARS Standard Output
 $varStdOk = "Tarea ejecutada exitosamente"
 $varStdError = "Ha ocurrido un error"
 
 #VARS 2x
-$var21 = "[1] - Bootear con TWRP"
+$var21 = "[1] - Iniciar con TWRP"
 $var22 = "[2] - Extraer boot.img"
 $var23 = "[3] - Subir boot.img"
 $var24 = "[4] - Bajar patched_boot.img"
@@ -22,7 +22,7 @@ $var24 = "[4] - Bajar patched_boot.img"
 #VARS 21x
 $var211 = "Presiona cualquier tecla para iniciar en modo fastboot"
 $var212 = "Cuando su móvil esté en modo fastboot presione cualquier tecla para continuar.."
-$var213 = "Booteando con TWRP, por favor espere.."
+$var213 = "Iniciando con TWRP, por favor espere.."
 
 #VARS 22x
 $var221 = "Extrayendo imagen boot.img, por favor espere.."
@@ -34,7 +34,7 @@ $var231 = "Subiendo imagen boot.img, por favor espere.."
 $var241 = "Extrayendo imagen boot.img, por favor espere.."
 
 #VARS 3x
-$var31 = "[1] - Rootear"
+$var31 = "[1] - Instalar root"
 
 #VARS 31x
-$var311 = "Rooteando su dispositivo, por favor espere.."
+$var311 = "Instalando root en su dispositivo, por favor espere.."
