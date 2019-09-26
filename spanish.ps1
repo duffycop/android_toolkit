410c1e37c57df73f7189667dba8ea1483f523de5
Tino <cmartinpf@gmail.com>
2019-09-26 18:45:26 -0300

    Create spanish.ps1

---
+++ spanish.ps1
@@ -0,0 +1,40 @@
+#HOME VARS
+$var0 = "[0] - Salir"
+$var1 = "[1] - Instalar drivers"
+$var2 = "[2] - Extraer boot"
+$var3 = "[3] - Rootear"
+$var4 = "[4] - Desrootear"
+$varChoice = "Por favor, elije una opción"
+
+#VARS 1x
+$varInstalling = "Instalando drivers, por favor espere.."
+
+#VARS Standard Output
+$varStdOk = "Tarea ejecutada exitosamente"
+$varStdError = "Ha ocurrido un error"
+
+#VARS 2x
+$var21 = "[1] - Bootear con TWRP"
+$var22 = "[2] - Extraer boot.img"
+$var23 = "[3] - Subir boot.img"
+$var24 = "[4] - Bajar patched_boot.img"
+
+#VARS 21x
+$var211 = "Presiona cualquier tecla para iniciar en modo fastboot"
+$var212 = "Cuando su móvil esté en modo fastboot presione cualquier tecla para continuar.."
+$var213 = "Booteando con TWRP, por favor espere.."
+
+#VARS 22x
+$var221 = "Extrayendo imagen boot.img, por favor espere.."
+
+#VARS 23x
+$var231 = "Subiendo imagen boot.img, por favor espere.."
+
+#VARS 24x
+$var241 = "Extrayendo imagen boot.img, por favor espere.."
+
+#VARS 3x
+$var31 = "[1] - Rootear"
+
+#VARS 31x
+$var311 = "Rooteando su dispositivo, por favor espere.."
