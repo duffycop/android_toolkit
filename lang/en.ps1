8a92f2c2e19ca9fb0d73fcbca5f2c58ab081b995
Tino <cmartinpf@gmail.com>
2019-09-26 18:53:52 -0300

    Create en.ps1

---
+++ lang/en.ps1
@@ -0,0 +1,40 @@
+#HOME VARS
+$var0 = "[0] - Exit"
+$var1 = "[1] - Install drivers"
+$var2 = "[2] - Extract boot"
+$var3 = "[3] - Root"
+$var4 = "[4] - Unroot"
+$varChoice = "Please, put your choice"
+
+#VARS 1x
+$varInstalling = "Installing drivers, please wait.."
+
+#VARS Standard Output
+$varStdOk = "Task executed successfully"
+$varStdError = "An error has occurred"
+
+#VARS 2x
+$var21 = "[1] - Boot with TWRP"
+$var22 = "[2] - Extract boot.img"
+$var23 = "[3] - Push boot.img"
+$var24 = "[4] - Pull patched_boot.img"
+
+#VARS 21x
+$var211 = "Press any key to start in fastboot"
+$var212 = "When your phone is in fastboot press any key to continue.."
+$var213 = "Booting with TWRP, please wait.."
+
+#VARS 22x
+$var221 = "Extracting boot.img, please wait.."
+
+#VARS 23x
+$var231 = "Pushing imagen boot.img, please wait.."
+
+#VARS 24x
+$var241 = "Pulling imagen patched_boot.img, please wait.."
+
+#VARS 3x
+$var31 = "[1] - Root"
+
+#VARS 31x
+$var311 = "Rooting your phone, please wait.."
